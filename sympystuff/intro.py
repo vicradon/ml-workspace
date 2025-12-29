c1a252664c1f2f9e691c1a9c1abbf9093d85a40d
osinachi.chukwujama@gmail.com <osinachi.chukwujama@gmail.com>
2025-12-29 11:26:35 +0000

    chore: add scikitlearn stuff

---
+++ sympystuff/intro.py
@@ -0,0 +1,13 @@
+from sympy import Symbol, Rational, Integer, cos
+
+x = Symbol("x")
+
+
+e = 1 / cos(x)
+
+
+a = Rational(5, 8)
+print("value of a is", str(a))
+
+b = Integer(3.579)
+print("value of b is", str(b))
